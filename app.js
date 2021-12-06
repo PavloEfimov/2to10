1771f0a03451828759d2c966e12f0b37baf48bbf
Pavlo Yefimov <pavlo.yefimov@gmail.com>
2021-12-06 23:15:11 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+let btn = document.querySelector('#button');
+let input = document.querySelector('#input');
+let inputBin = document.querySelector('#inputBin');
+let output = document.querySelector('#output');
+let binNumber='';
+
+
+btn.addEventListener('click', convert2to10);
+input.addEventListener('keydown', checkInput)
+
+function checkInput (e){
+    if ((e.key!=='0')&&(e.key!=='1')) {
+        alert ('введенный символ - не двоичное число');
+    
+    } else if (binNumber.length>8){
+        alert ('длина двоичного числа превышает 8')
+    } else {
+        binNumber = binNumber + e.key
+    }
+   input.value = '';
+   inputBin.textContent = binNumber;
+};
+function convert2to10() {
+    let number10=0;
+    for (let i = binNumber.length-1; i>=0; i--){
+        number10 =number10 + binNumber[binNumber.length-1-i]*Math.pow(2, i)
+    }
+    console.log ('number10 ',number10)
+    output.textContent = number10;
+    return null;
+};
